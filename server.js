276d86145317fe63c4e0f4c11c45db6527edb554
Nuarc Fintech <ops@shipinfra.com>
2026-05-02 04:06:54 +0530

    fix proxy and iframe src

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 const app = express();
- 
-app.use(express.static('public'));
- 
-app.use('/forms', createProxyMiddleware({
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const proxyOptions = {
   target: 'https://tools.investwellonline.com',
   changeOrigin: true,
-  onProxyRes: (proxyRes) => {
-    delete proxyRes.headers['x-frame-options'];
-    delete proxyRes.headers['content-security-policy'];
+  secure: false,
+  selfHandleResponse: false,
+  on: {
+    proxyRes: function (proxyRes) {
+      delete proxyRes.headers['x-frame-options'];
+      delete proxyRes.headers['X-Frame-Options'];
+      delete proxyRes.headers['content-security-policy'];
+      delete proxyRes.headers['Content-Security-Policy'];
+      proxyRes.headers['access-control-allow-origin'] = '*';
+    },
+    error: function (err, req, res) {
+      res.status(500).send('Proxy error: ' + err.message);
+    }
   }
-}));
- 
-app.listen(3000, () => console.log('Running!'));
+};
+
+app.use('/forms', createProxyMiddleware(proxyOptions));
+app.use('/assets', createProxyMiddleware(proxyOptions));
+app.use('/static', createProxyMiddleware(proxyOptions));
+app.use('/api', createProxyMiddleware(proxyOptions));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Nuarc Fintech Portal running on port ' + PORT));
